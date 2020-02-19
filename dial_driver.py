f5c2684f59abf3b868c909434cc1c621605a01f8
Mauricio Bustos <m@bustos.org>
2020-02-18 18:47:38 -0800

    tabs

--- dial_driver.py
+++ dial_driver.py
@@ -37,26 +37,26 @@ def mapFromTo(x, a, b, c, d):
 
 def handle_pressure(unused_addr, args):
    """ Handle the update from the pressure sensor """
-    try:
-        logger.info(f'[{args}]')
-        pwm.setServoPulse(15, int(mapFromTo(args, -15000, 15000, 500, 2500)))
-    except ValueError as e:
-        logger.error(e)
-        
+   try:
+      logger.info(f'[{args}]')
+      pwm.setServoPulse(15, int(mapFromTo(args, -15000, 15000, 500, 2500)))
+   except ValueError as e:
+      logger.error(e)
+      
 if __name__ == "__main__":
-    pwm = PCA9685(0x40, debug=False)
-    pwm.setPWMFreq(50)
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--ip", default="10.0.1.58", help="The ip to listen on")
-    parser.add_argument("--port", type=int, default=10003, help="The port to listen on")
-    args = parser.parse_args()
-    
-    dispatcher = dispatcher.Dispatcher()
-    dispatcher.map("/pressure", handle_pressure)
-    
-    server = osc_server.ThreadingOSCUDPServer((args.ip, args.port), dispatcher)
-    logger.info(f'Serving on {server.server_address}')
-    server.serve_forever()
-    
+   pwm = PCA9685(0x40, debug=False)
+   pwm.setPWMFreq(50)
+   
+   parser = argparse.ArgumentParser()
+   parser.add_argument("--ip", default="10.0.1.58", help="The ip to listen on")
+   parser.add_argument("--port", type=int, default=10003, help="The port to listen on")
+   args = parser.parse_args()
+   
+   dispatcher = dispatcher.Dispatcher()
+   dispatcher.map("/pressure", handle_pressure)
+   
+   server = osc_server.ThreadingOSCUDPServer((args.ip, args.port), dispatcher)
+   logger.info(f'Serving on {server.server_address}')
+   server.serve_forever()
+   
  
